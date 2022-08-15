93a3d4d783e041c74efe512dc1d912ca25103279
fleetimee <novian.andika@students.amikom.ac.id>
2022-08-15 21:40:30 +0700

    test

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'app/routes/app_pages.dart';
 void main() {
   runApp(
     GetMaterialApp(
-      title: "Application",
+      title: "Punya fleetime",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
     ),
